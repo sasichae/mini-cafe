d8aa42ccae7d9a4f7dae637a9de6bbda55d7bb0a
Sasiwimol Chaesong <sasiwimol.maill@gmail.com>
2026-09-11 02:07:57 +0700

    Remove backend files and add draft for Mini Café Backend development

--- backend/src/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const {getProduct} = require("../controllers/product.controller");
-const router = express.Router();
-
-router.get("/", getProduct);
-
-module.exports = router;
